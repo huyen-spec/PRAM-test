87c79fff74799d000bd726ba82e049cdec028c9b
huyennt <huyen2wes@gmail.com>
2023-06-18 09:48:51 +0700

    first commit

---
+++ __init__.py
@@ -0,0 +1,13 @@
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = "unknown"
+
+
+# __all__ = (
+#     "napari_get_reader",
+#     "write_single_image",
+#     "write_multiple",
+#     "ExampleQWidget",
+#     "example_magic_widget",
+# )
